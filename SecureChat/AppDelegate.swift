97ce30a68beeec8e5574b771caea78b1571d5d31
NetDemon <netdemon@NetDemons-iMac.local>
2015-09-08 23:05:17 +0800

    Initial Commit

---
+++ SecureChat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SecureChat
+//
+//  Created by NetDemon on 15/9/8.
+//  Copyright (c) 2015年 20CN. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
